ef990ea124bf14a55014961887a8452e5b7d2a03
khouloud Haddad Amamou <khouloud.haddad@gmail.com>
2024-03-24 12:33:53 +0100

    Create unit tests as per requirements

--- src/Sales/Cart.php
+++ src/Sales/Cart.php
@@ -1,8 +1,44 @@
 <?php
-
+declare(strict_types=1);
 namespace khoul\Sales;
 
-class Cart
-{
+use PhpParser\Node\Expr\Array_;
 
+final class Cart
+{
+    private string $id;
+    private array $items;
+
+    public function __construct()
+    {
+        $this->id = sha1(uniqid('', true));
+        $this->items = [];
+    }
+
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    public function getItems(): array
+    {
+        return $this->items;
+    }
+
+    public function addItem(string $sku, string $name, $price, $quantity): self
+    {
+        $this->items[$sku] = [
+            'sku' => $sku,
+            'name' => $name,
+            'price' => $price,
+            'ordered_quantity' => $quantity
+        ];
+
+        return $this;
+    }
+
+    public function getItem(string $sku): ?array
+    {
+        return $this->items[$sku];
+    }
 }
