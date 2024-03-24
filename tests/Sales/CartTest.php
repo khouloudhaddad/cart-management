96255443db4ceeae1eb31c46ff2452da283fb1d0
khouloud Haddad Amamou <khouloud.haddad@gmail.com>
2024-03-24 11:45:11 +0100

    Empty passing test & PHP unit config

--- tests/Sales/CartTest.php
+++ tests/Sales/CartTest.php
@@ -1,8 +1,15 @@
 <?php
 
-namespace Sales;
+declare(strict_types=1);
 
-class CartTest
+namespace khoul\Tests\Sales;
+
+use PHPUnit\Framework\TestCase;
+
+final class CartTest extends TestCase
 {
-
+    public function test_is_true(): void
+    {
+        $this->assertTrue(true);
+    }
 }
